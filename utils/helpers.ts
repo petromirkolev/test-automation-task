816254b266330f76f611b1e82bcb2f9978802e80
Petromir Kolev <petromirkolev@gmail.com>
2026-04-17 14:09:52 +0300

    feat: Add create existing email account helper

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,5 +1,16 @@
+import { EmailAccountsPage } from '../pages/email-accounts.page';
+import { ACCOUNT_NAME, SELECTED_DOMAIN } from './constants';
+
 export function decodeJwtPayload(token: string) {
   const payload = token.split('.')[1];
   const base64 = payload.replace(/-/g, '+').replace(/_/g, '/');
   return JSON.parse(Buffer.from(base64, 'base64').toString('utf-8'));
+}
+
+export async function createExistingAccount(
+  emailAccountsPage: EmailAccountsPage,
+): Promise<void> {
+  await emailAccountsPage.createAccount(SELECTED_DOMAIN, ACCOUNT_NAME);
+  await emailAccountsPage.expectSuccessMessage(ACCOUNT_NAME);
+  await emailAccountsPage.clickBackButton();
 }
